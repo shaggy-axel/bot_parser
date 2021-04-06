68765bdad6d6f627c8bcabddc6571d6f7da2362e
shaggy-axel <kornerus@fjfi.cvut.cz>
2021-04-06 23:35:25 +0300

    add parser and notify about games

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 from sqlighter import SQLighter
+from stopgame import StopGame
 
 logging.basicConfig(filename="bot.log", level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -15,6 +16,10 @@ dp = Dispatcher(bot)
 #Инициализируем базу данных
 db = SQLighter('bot_db')
 
+# инициализируем парсер
+sg = StopGame('lastkey.txt')
+
+poll = 10
 
 @dp.message_handler(commands=['subscribe'])
 async def subscribe(message: types.Message):
@@ -42,6 +47,39 @@ async def unsubscribe(message: types.Message):
 	await message.answer("Вы отписались от уведомлений")
 
 
+# проверяем наличие новых игр и делаем рассылки
+async def scheduled(wait_for):
+	while True:
+		await asyncio.sleep(wait_for)
+
+		# проверяем наличие новых игр
+		new_games = sg.new_games()
+
+		if(new_games):
+			# если игры есть, переворачиваем список и итерируем
+			new_games.reverse()
+			for ng in new_games:
+				# парсим инфу о новой игре
+				nfo = sg.game_info(ng)
+
+				# получаем список подписчиков бота
+				subscriptions = db.get_subscriptions()
+
+				# отправляем всем новость
+				with open(sg.download_image(nfo['image']), 'rb') as photo:
+					for s in subscriptions:
+						await bot.send_photo(
+							s[1],
+							photo,
+							caption = nfo['title'] + "\n" + "Оценка: " + nfo['score'] + "\n" + nfo['excerpt'] + "\n\n" + nfo['link'],
+							disable_notification = True
+						)
+				
+				# обновляем ключ
+				sg.update_lastkey(nfo['id'])
+
+
 if __name__ == '__main__':
+	dp.loop.create_task(scheduled(poll)) # пока что оставим 10 секунд (в качестве теста)	
 	executor.start_polling(dp, skip_updates=True)
 
